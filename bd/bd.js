d83ede706819f67fc1e9e72b6a02a6b0de9a5685
Juan Minu <arcangeljuan1@unicauca.edu.co>
2019-06-07 17:33:26 -0500

    sf
    df

--- bd/bd.js
+++ bd/bd.js
@@ -6,46 +6,7 @@ const bd_libros = [{ "_id": 1, "title": "Crimen y Castigo", "isbn": "1933988273"
 { "_id": 3, "title": "Cien Años de Soledad", "isbn": "1933288673", "pageCount": 492, "thumbnailUrl": "https://static.abebookscdn.com/cdn/es/imagenes/100-mejores-libros/100soledad.jpg", "authors": "Gabriel Garcia Marquez", "categories": "Narrativa" }
 ];
 
-var usuarios = [
-    {
-      "userId": "rirani",
-      "jobTitleName": "Developer",
-      "firstName": "Romin",
-      "lastName": "Irani",
-      "preferredFullName": "Romin Irani",
-      "employeeCode": "E1",
-      "region": "CA",
-      "phoneNumber": "408-1234567",
-      "emailAddress": "romin.k.irani@gmail.com",
-      "contrasenia": 123,
-      "rol": "estudiante"
-    },
-    {
-      "userId": "nirani",
-      "jobTitleName": "Developer",
-      "firstName": "Neil",
-      "lastName": "Irani",
-      "preferredFullName": "Neil Irani",
-      "employeeCode": "E2",
-      "region": "CA",
-      "phoneNumber": "408-1111111",
-      "emailAddress": "neilrirani@gmail.com",
-      "contrasenia": 123,
-      "rol": "estudiante"
-    },
-    {
-      "userId": "thanks",
-      "jobTitleName": "Program Directory",
-      "firstName": "Tom",
-      "lastName": "Hanks",
-      "preferredFullName": "Tom Hanks",
-      "employeeCode": "E3",
-      "region": "CA",
-      "phoneNumber": "408-2222222",
-      "emailAddress": "tomhanks@gmail.com",
-      "rol": "admin"
-    }
-  ];
+
 
 var reservas = [];
 
